25e0897cc9991451d964893878e7fd568e4579e2
gaoting <382470029@qq.com>
2017-01-09 17:24:06 +0800

    add new pages

---
+++ src/app/pages/raise-personal/raise-personal.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: '',
+    templateUrl: 'raise-personal.template.html',
+    styleUrls: ['raise-personal.style.css']
+})
+export class PersonalComponent {}
